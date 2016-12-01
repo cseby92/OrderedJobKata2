f770be0e73ec8a8ef47381aa8dad3a9042194c0a
Sebastian Chikan <cseby92@gmail.com>
2016-12-01 22:34:04 +0100

    All tasks done

--- index.js
+++ index.js
@@ -1,1 +1,158 @@
 'use strict'
+
+const JOB_SEPARATOR = '=>';
+const MULTIPLE_JOBS_SEPARATOR = '\n';
+const NAME_INDEX = 0;
+const DEPEPENDENCY_INDEX = 5;
+
+
+class OrderJobsCommand {
+
+    constructor(expression) {
+        this._expression = expression || '';
+    }
+
+    execute() {
+        return JobRepository.fromExpression(this._expression)
+            .sortByDependency()
+            .getNames()
+            .join('');
+    }
+
+    static create(expression) {
+        return new OrderJobsCommand(expression);
+    }
+}
+
+class JobRepository {
+
+    constructor(jobs = []) {
+        this._jobs = jobs;
+    }
+
+    getNames() {
+        return this._jobs.map((job) => job.name);
+    }
+
+    sortByDependency() {
+
+        let ordered = new JobRepository();
+
+        this._jobs.forEach((job) => {
+
+            ordered._addJob(job);
+            if (job.hasDependency()) {
+                this._checkForSelfReference(job);
+                this._checkForCircularDependencyChain(job);
+                ordered._insertBefore(this._getJobByName(job.dependency), job);
+
+            }
+        });
+
+        return ordered;
+    }
+
+    static fromExpression(expression) {
+        let jobs = expression.split(MULTIPLE_JOBS_SEPARATOR).map(Job.fromExpression);
+        return new JobRepository(jobs);
+    }
+
+    _checkForCircularDependencyChain(inJob){
+        
+        let cJobIndex = this._jobs.indexOf(inJob);
+        let cJob = new Job({name: inJob.name, dependency: inJob.dependency});
+
+        /*this._jobs.forEach((job, index) => {
+            if(cJobIndex !== index && job.hasDependency()){
+                if(cJob.dependency === job.name){
+                    cJob.dependency = job.dependency;
+
+
+                } 
+            }
+                                if(cJob.name === "b")
+                    console.log(cJob);
+                    
+                if(cJob.hasSelfReference())
+                    throw new Error("Jobs can't have circular dependencies");
+        });*/
+        this._jobs.forEach((job, index) => {
+            if(cJobIndex !== index && job.hasDependency()){
+                if(cJob.dependency === job.name){
+                    cJob.dependency = job.dependency;
+
+                if(cJob.hasSelfReference())
+                    throw new Error("Jobs can't have circular dependencies");
+                }
+            }
+        });
+
+                
+
+    }
+
+    _checkForSelfReference(job) {
+        if (job.hasSelfReference()){
+            throw new Error("Jobs can’t depend on themselves");
+        }
+            
+    }
+
+    _addJob(job) {
+        if (!this._jobExists(job))
+            this._jobs.push(job);
+    }
+
+    _insertBefore(job, before) {
+        if (!this._jobExists(job)) {
+            let index = this._jobs.indexOf(before);
+            this._jobs.splice(index, 0, job);
+        }
+    }
+
+    _jobExists(job) {
+        return Boolean((this._getJobByName(job.name)));
+    }
+
+    _getJobByName(jobName) {
+        return this._jobs.find((job) => {
+            if (job.name === jobName) return job;
+        });
+    }
+}
+
+class Job {
+
+    constructor({ name, dependency }) {
+        this.name = name;
+        this.dependency = dependency;
+    }
+
+    static fromExpression(expression) {
+        return new Job({
+            name: Job._fetchNameFromExpression(expression),
+            dependency: Job._fetchDependencyFromExpression(expression)
+        });
+    }
+
+    hasSelfReference() {
+        if(this.name != '' && this.name === this.dependency)
+            return true;
+        return false;
+    }
+
+    hasDependency() {
+        return Boolean(this.dependency);
+    }
+
+    static _fetchNameFromExpression(expression) {
+
+        return expression.charAt(NAME_INDEX);
+    }
+    static _fetchDependencyFromExpression(expression) {
+
+        return expression.charAt(DEPEPENDENCY_INDEX) || '';
+    }
+}
+
+module.exports = OrderJobsCommand;
--- index.spec.js
+++ index.spec.js
@@ -1,1 +1,61 @@
 'use strict'
+
+const expect = require('chai').expect;
+const OrderJobsCommand = require('./index');
+
+describe('OrderJobsCommand', function () {
+
+    let order = (jobs) => OrderJobsCommand.create(jobs).execute();
+
+
+    describe('#execute', () => {
+        it('should return empty string for empty input', () => {
+            expect(order('')).to.eql('');
+        });
+
+        it('should return the job if only is in the input', () => {
+            expect(order('a =>')).to.eql('a');
+            expect(order('b =>')).to.eql('b');
+        });
+
+        it('should return the jobs in any order if multiple jobs are given without dependency', () => {
+            let jobs = order('a =>\nb =>\nc =>');
+
+            expect(jobs.length).to.eql(3);
+            expect(jobs).to.contain('a');
+            expect(jobs).to.contain('b');
+            expect(jobs).to.contain('c');
+        });
+
+        it('should return the jobs in order if multiple jobs are given with dependency', () => {
+            let jobs = order('a =>\nb => c\nc =>');
+
+            expect(jobs.indexOf('c')).to.below(jobs.indexOf('b'));
+
+        });
+
+        it('should return the jobs in order if multiple jobs are given with multiple dependency', () => {
+            let jobs = order('a =>\nb => c\nc => f\nd => a\ne => b\nf =>');
+
+            expect(jobs.indexOf('c')).to.below(jobs.indexOf('b'));
+            expect(jobs.indexOf('f')).to.below(jobs.indexOf('c'));
+            expect(jobs.indexOf('a')).to.below(jobs.indexOf('d'));
+            expect(jobs.indexOf('b')).to.below(jobs.indexOf('e'));
+
+        });
+
+        it('should return an Error: "Jobs can’t depend on themselves" for a self reference', function () {
+
+            expect(function () {
+                let jobs = order('a =>\nb =>\nc => c');
+            }).to.throw(Error, "Jobs can’t depend on themselves");
+        });
+
+        it('should return an Error: "Jobs can’t depend on themselves" for a self reference', function () {
+
+            expect(function () {
+                let jobs = order('a =>\nb => c\nc => f\nd => a\ne =>\nf => b');
+            }).to.throw(Error, 'Jobs can\'t have circular dependencies');
+        });
+    });
+});
